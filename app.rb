e7ea6e1940751c24bba8a41673b190dfa759b9dc
johnnyknoxville1337 <johnnyknoxville1337@outlook.com>
2018-08-24 13:33:09 +0000

    Ready for Heroku

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'sinatra/base'
 require 'sinatra/flash'
 require './lib/hangperson_game.rb'
+require 'sinatra'
+
+class MyApp < Sinatra::Base
+  get '/' do
+    "<!DOCTYPE html><html><head></head><body><h1>aloha Hawaii</h1></body></html>"
+  end
+end
 
 class HangpersonApp < Sinatra::Base
 
--- config.ru
+++ config.ru
@@ -1,3 +1,5 @@
 require './app'
 run HangpersonApp
 
+require './app'
+run MyApp
